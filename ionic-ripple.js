1310accac3bdbe9fc073b89c138c8a8630ef5507
Vitaliy Bobrov <bobrov1989@gmail.com>
2016-03-15 15:38:31 +0200

    Add ion-ripple directive

--- ionic-ripple.js
+++ ionic-ripple.js
@@ -2,7 +2,9 @@
   'use strict';
 
   angular.module('ionicRipple', ['ionic'])
-    .directive(ionRipple, ionRipple);
+    .directive('ionRipple', ionRipple);
+
+  ionRipple.$inject = [];
 
   function ionRipple() {
     var directive = {
@@ -12,61 +14,64 @@
 
     return directive;
 
-    function link(scope, element, attrs) {
+    function link(scope, element, attr) {
       var x;
       var y;
       var size;
       var offsets;
+      var rippleClass = 'ion-ripple';
+      var animateClassName = 'ion-ripple_animate';
 
       element.on('touchend mouseup', rippleHandler);
 
       //remove the event listener on scope destroy
-      scope.$on('$destroy', destroy);
+      scope.$on('$destroy', function() {
+        element.off('touchend mouseup', rippleHandler);
+      });
 
-      function rippleHandler(e) {
-        var ripple = this.querySelector('.angular-ripple');
-        var eventType = e.type;
+      function rippleHandler(event) {
+        var ripple = element[0].querySelector('.' + rippleClass);
         // Ripple
-        if (ripple === null) {
+        if (!ripple) {
           // Create ripple
           ripple = document.createElement('span');
-          ripple.className += ' angular-ripple';
+          ripple.classList.add(rippleClass);
 
           // Prepend ripple to element
-          this.insertBefore(ripple, this.firstChild);
+          element[0].insertBefore(ripple, element[0].firstChild);
 
           // Set ripple size
           if (!ripple.offsetHeight && !ripple.offsetWidth) {
             size = Math.max(element[0].offsetWidth, element[0].offsetHeight);
-            ripple.style.width = size + 'px';
-            ripple.style.height = size + 'px';
+            ripple.style.width = ripple.style.height = size + 'px';
           }
         }
-
         // Remove animation effect
-        ripple.className = ripple.className.replace(/ ?(animate)/g, '');
+        ripple.classList.remove(animateClassName);
 
-        // get click coordinates by event type
-        if (eventType === 'mouseup') {
-          x = e.pageX;
-          y = e.pageY;
-        } else if (eventType === 'touchend') {
-          try {
-            var origEvent;
+        switch (event.type) {
+          case 'mouseup':
+            x = event.pageX;
+            y = event.pageY;
+            break;
+          case 'touchend':
+            try {
+              var origEvent;
 
-            if (typeof e.changedTouches !== 'undefined') {
-              origEvent = e.changedTouches[0];
-            } else {
-              origEvent = e.originalEvent;
+              if (typeof event.changedTouches !== 'undefined') {
+                origEvent = event.changedTouches[0];
+              } else {
+                origEvent = event.originalEvent;
+              }
+
+              x = origEvent.pageX;
+              y = origEvent.pageY;
+            } catch (e) {
+              // fall back to center of el
+              x = ripple.offsetWidth / 2;
+              y = ripple.offsetHeight / 2;
             }
-
-            x = origEvent.pageX;
-            y = origEvent.pageY;
-          } catch (e) {
-            // fall back to center of el
-            x = ripple.offsetWidth / 2;
-            y = ripple.offsetHeight / 2;
-          }
+            break;
         }
 
         // set new ripple position by click or touch position
@@ -80,15 +85,11 @@
         }
 
         offsets = getPos(element[0]);
-        ripple.style.left = (x - offsets.left - size / 2) + 'px';
         ripple.style.top = (y - offsets.top - size / 2) + 'px';
+        ripple.style.left = (x - offsets.left - size / 2) + 'px';
 
         // Add animation effect
-        ripple.className += ' animate';
-      }
-
-      function destroy() {
-        element.off('touchend mouseup', rippleHandler);
+        ripple.classList.add(animateClassName);
       }
     }
   }
